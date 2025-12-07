69dc80d2a2c2f12531f8ee266181f9f564dde0de
fgnm <fgnm.dev@gmail.com>
2025-12-07 23:57:25 +0100

    huge refactor of box2dlights to support light batching and finalized pseudo3d effect

--- src/main/java/games/rednblack/h2d/extension/spine/SpineComponentFactory.java
+++ src/main/java/games/rednblack/h2d/extension/spine/SpineComponentFactory.java
@@ -6,7 +6,7 @@ import com.artemis.EntityTransmuterFactory;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 import com.esotericsoftware.spine.*;
-import games.rednblack.editor.renderer.box2dLight.RayHandler;
+import games.rednblack.editor.renderer.lights.RayHandler;
 import games.rednblack.editor.renderer.components.DimensionsComponent;
 import games.rednblack.editor.renderer.components.normal.NormalMapRendering;
 import games.rednblack.editor.renderer.data.MainItemVO;
